6e5ffb6d771b2faeb5c2df3b209c129b79033d05
Bmijin <bmj13465@jtts.co.kr>
2024-10-16 13:17:38 +0900

    상품 Entity 비즈니스 추가

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,8 +27,23 @@ public abstract class Item {
     private List<Category> categories = new ArrayList<>();
 
     // 비즈니스 로직
-    // data(stockQuantity(재고 수량))를 가지고 있는 곳에서 비즈니스 로직이 나가는게 좋다.
+    // data(stockQuantity(재고 수량))를 가지고 있는 곳에서 비즈니스 로직이 나가는게 응집력이 좋다.
+
+    /**
+     * 재고 수량 증가
+     */
     public void addStock(int quantity) {
         this.stockQuantity += quantity;
+    }
+
+    /**
+     * 재고 수량 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
     }
 }
