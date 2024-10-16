62718742cae64bac643f4c59c624f3328adfc0aa
Bmijin <bmj13465@jtts.co.kr>
2024-10-16 15:40:57 +0900

    주문 검색 기능 개발

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -21,5 +21,14 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    // public List<Order> findAll(OrderSearch orderSearch) {}
+    public List<Order> findAll(OrderSearch orderSearch) {
+        // 동적 쿼리는 Querydsl 라이브러리 사용하는 게 가장 좋다. (JPQL 로 처리하거나, JPA Criteria 로 처리하면 오타 위험성 및 가독성 저하 문제가 상당하다. 사용 xxx)
+        return em.createQuery("select o from Order o join o.member m" +
+                " where o.status = :status" +
+                " and m.name like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setMaxResults(1000)        // 최대 1000건
+                .getResultList();
+    }
 }
