e6c1ab910951a93d2ffd6b2bb93c84861f5c5e67
Bmijin <bmj13465@jtts.co.kr>
2024-10-18 11:31:25 +0900

    회원 조회 API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -9,6 +9,9 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController     // @Controller 와 @ResponseBody 가 하나에 포함되어 있다.
 @RequiredArgsConstructor
 public class MemberApiController {
@@ -16,10 +19,41 @@ public class MemberApiController {
     private final MemberService memberService;
 
     /*
+    * v1의 경우, Member 의 모든 속성 값이 다 노출된다. 특정 속성을 노출시키지 않으려면 Member.java Entity 에서 해당 속성에 @JsonIgnore 이라는 어노테이션을 추가해야된다.
+    * v2의 경우, 노출하고 싶은 것만 DTO 로 생성해서 노출할 수 있다. */
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+        return new Result(collect.size(), collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+
+    // object 로( { data: [] } 로 한번 감싸주기 위해 Result 필요
+    // count 추가하고 memberV2 메서드에 collect.size() 하면 count, data 두가지를 반환해준다.
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    /*
     * v1의 경우, Member Entity 에서 name 속성을 username 이라고 변경하게 되면 Front 에서 api 호출 시, NotEmpty 애러를 만나게 된다. & Entity 에 @NotEmpty 를 사용해서 모든 API 에서 필수 값이 된다.
     * v2의 경우, 속성 이름을 변경하면 saveMemberV2 메서드에서 컴파일 오류가 나기때문에 API 배포 전 오류를 잡을 수 있다는 것이 장점이다. ⇒ API 스펙이 달라지지 않는다. & API 마다 @NotEmpty 사용할 수도 사용 안할 수도 있다.
     * ⭐API 는 등록, 요청 모두 절대 Entity 를 사용하지 말자. DTO 사용 */
-    
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
         Long id = memberService.join(member);
