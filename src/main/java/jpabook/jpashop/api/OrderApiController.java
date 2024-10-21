d625719730c4e61cc152a91a8d44a923437b8075
Bmijin <bmj13465@jtts.co.kr>
2024-10-21 13:49:19 +0900

    (컬렉션 조회)주문 조회 (Entity 직접 노출)

---
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+
+        for (Order order : all) {
+            // 강제 초기화 ⇒ 실제로 해당 값이 필요할 때 추가적인 데이터베이스 쿼리가 발생하지 않도록 하기 위해 사용(미리 로드)
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+
+        return all;
+    }
+}
