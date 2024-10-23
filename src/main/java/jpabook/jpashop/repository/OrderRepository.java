804f7cbce58a5930fae2b6e3068c7928c82b15d9
Bmijin <bmj13465@jtts.co.kr>
2024-10-23 10:53:22 +0900

    QueryDSL 사용

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,9 +1,14 @@
 package jpabook.jpashop.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.*;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.QMember;
+import jpabook.jpashop.domain.QOrder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -25,7 +30,7 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    public List<Order> findAll(OrderSearch orderSearch) {
+    /*public List<Order> findAll(OrderSearch orderSearch) {
         // 동적 쿼리는 Querydsl 라이브러리 사용하는 게 가장 좋다. (JPQL 로 처리하거나, JPA Criteria 로 처리하면 오타 위험성 및 가독성 저하 문제가 상당하다. 사용 xxx)
         return em.createQuery("select o from Order o join o.member m" +
                 " where o.status = :status" +
@@ -34,7 +39,7 @@ public class OrderRepository {
                 .setParameter("name", orderSearch.getMemberName())
                 .setMaxResults(1000)        // 최대 1000건
                 .getResultList();
-    }
+    }*/
 
     public List<Order> findAllByString(OrderSearch orderSearch) {
 
@@ -102,6 +107,38 @@ public class OrderRepository {
         cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
         TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
         return query.getResultList();
+    }
+
+    /*
+    * Querydsl 사용
+    * 꼭 동적 쿼리가 아니여도 정적 쿼리인 경우에도 Querydsl 을 사용하는 것이 좋다.
+    * 직관적인 문법, 컴파일 시점에 빠른 문법 오류 발견, 코드 자동완성, 코드 재사용(이것은 자바다), JPQL new 명령어와는 비교가 안될 정도로 깔끔한 DTO 조회를 지원한다.*/
+    public List<Order> findAll(OrderSearch orderSearch) {
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
+
+        return query
+                .select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()), nameLike(orderSearch.getMemberName()))
+                .limit(1000)
+                .fetch();
+    }
+
+    private static BooleanExpression nameLike(String memberName) {
+        if (memberName == null) {
+            return null;
+        }
+        return QMember.member.name.like(memberName);
+    }
+
+    private BooleanExpression statusEq(OrderStatus statusCond) {
+        if (statusCond == null) {
+            return null;
+        }
+        return QOrder.order.status.eq(statusCond);
     }
 
     // 패치 조인은 JPA 실무에서 정말 많이 쓰이기 때문에 100% 이해해야 한다.
